2d6f7b822a04d193031f6feb6022e974ebf15200
buckhx <buckhx@gmail.com>
2015-12-21 18:24:17 +0000

    adding version.go again

---
+++ resources/version.go
@@ -0,0 +1,21 @@
+// Resources is a package for containing string resources about how to run diglet.
+// Files generated from scripts/include.go will end up here
+package resources
+
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+)
+
+func Version() string {
+	cmd := exec.Command("git", "describe", "--always")
+	var ver bytes.Buffer
+	cmd.Stdout = &ver
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimSpace(ver.String())
+	//TODO add a '{+n}' to version if git diff --numstat isn't empty
+}
