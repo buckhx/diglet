168a7093b2ce688b0265d16511f6eb256b9998d0
buckhx <buckhx@gmail.com>
2016-01-19 03:40:33 +0000

    added filter

--- mbt/csv.go
+++ mbt/csv.go
@@ -3,6 +3,7 @@ package mbt
 import (
 	"encoding/csv"
 	"github.com/buckhx/diglet/util"
+	"github.com/deckarep/golang-set"
 	"io"
 	"os"
 	"strconv"
@@ -22,13 +23,26 @@ type CsvSource struct {
 	path      string
 	headers   map[string]int
 	delimiter string
+	filter    mapset.Set
 	fields    GeoFields
 }
 
-func NewCsvSource(path, delimiter string, fields GeoFields) *CsvSource {
+func NewCsvSource(path string, filter []string, delimiter string, fields GeoFields) *CsvSource {
+	var set mapset.Set
+	if filter == nil {
+		set = nil
+	} else {
+		set = mapset.NewSet()
+		for _, k := range filter {
+			set.Add(k)
+		}
+		set.Add(fields["lat"])
+		set.Add(fields["lon"])
+	}
 	return &CsvSource{
 		path:      path,
 		delimiter: delimiter,
+		filter:    set,
 		fields:    fields,
 	}
 }
@@ -58,23 +72,29 @@ func (c *CsvSource) publishLines() (lines chan []string, err error) {
 	if err != nil {
 		return
 	}
-	defer f.Close()
 	reader := csv.NewReader(f)
-	c.headers = readHeaders(reader)
+	c.headers = readHeaders(reader, c.filter)
 	err = c.fields.Validate() //c.headers)
 	//TODO if err != nil
 	lines = make(chan []string, 100)
 	go func() {
 		defer close(lines)
+		defer f.Close()
 		for {
 			line, err := reader.Read()
 			if err == io.EOF {
 				break
 			}
 			if err != nil {
+				panic(err)
 				util.Warn(err, "line reading")
+			} else if line[c.headers[c.fields["lat"]]] == "" {
+				continue
+				//err = util.Errorf("No coordinates %v", line)
+				//util.Warn(err, "no lat/lon")
+			} else {
+				lines <- line
 			}
-			lines <- line
 		}
 	}()
 	return
@@ -82,13 +102,15 @@ func (c *CsvSource) publishLines() (lines chan []string, err error) {
 
 func (c *CsvSource) featureAdapter(line []string) (feature *Feature, err error) {
 	feature = NewFeature("point")
-	props := make(map[string]interface{})
+	props := make(map[string]interface{}) // TODO alloc correct amount
 	for k, i := range c.headers {
+		// biggest mem allocation
 		props[k] = line[i]
 	}
 	feature.Properties = props
 	lat, err := strconv.ParseFloat(line[c.headers[c.fields["lat"]]], 64)
 	if err != nil {
+		util.Info("%v", line)
 		return nil, err
 	}
 	lon, err := strconv.ParseFloat(line[c.headers[c.fields["lon"]]], 64)
@@ -100,14 +122,18 @@ func (c *CsvSource) featureAdapter(line []string) (feature *Feature, err error) 
 	return
 }
 
-func readHeaders(reader *csv.Reader) (headers map[string]int) {
+func readHeaders(reader *csv.Reader, filter mapset.Set) (headers map[string]int) {
 	line, err := reader.Read()
 	util.Warn(err, "reading headers")
 	headers = make(map[string]int, len(line))
 	for i, k := range line {
 		//if _, ok := c.fields[k]; !ok {
-		headers[k] = i
+		k = util.Slugged(k, "_")
+		if filter == nil || filter.Contains(k) {
+			headers[k] = i
+		}
 		//}
 	}
+	util.Debug("Headers %v", headers)
 	return
 }
