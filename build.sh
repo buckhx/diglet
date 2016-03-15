afb9962abfca38748ff11b97b0f40de8b7fca489
buckhx <buckhx@gmail.com>
2016-03-15 18:08:36 +0000

    added build.sh for reals

---
+++ build.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+repeat=$(printf "%40s")
+function header {
+    echo "${repeat// /*} $1 ${repeat// /*}"
+}
+
+header "go get"
+go get github.com/fzipp/gocyclo
+go get github.com/client9/misspell/cmd/misspell
+go get github.com/golang/lint/golint
+go get ./...
+
+header "style checks"
+header "misspell"
+echo "misspell ./**/*"
+misspell ./**/*
+
+header "go lint"
+echo golint ./...
+golint ./...
+
+header "go vet"
+echo go tool vet ./
+go tool vet ./
+
+header "go cyclo"
+echo gocyclo -over 10 ./
+gocyclo -over 10 ./
+
+header " go test"
+echo go test ./...
+go test ./...
+
+header "go build"
+echo go build -ldflags '-extldflags "-static"' -o diglet
+go build -ldflags '-extldflags "-static"' -o diglet
+
+echo "Done!"
